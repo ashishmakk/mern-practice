48da12ffba70bfcab0954ad2c1359c0ed6201f75
Ashish Makwana <ashishn.makwana@gmail.com>
2024-12-18 22:19:01 +0530

    pagination complete

--- client/src/components/PaginationContainer.jsx
+++ client/src/components/PaginationContainer.jsx
@@ -1,33 +1,60 @@
-import React from "react";
 import { useAllJobsContext } from "../pages/AllJobs";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function PaginationContainer() {
   const { data } = useAllJobsContext();
   const { totalJobs, totalPages, currentPage, allJobs } = data;
 
   const pageArr = Array.from({ length: totalPages }, (_, index) => index + 1);
+  const navigate = useNavigate();
+
+  const { search, pathname } = useLocation();
+
+  const handlePageChange = (pageNum) => {
+    const searchParams = new URLSearchParams(search);
+    searchParams.set("page", pageNum);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
 
   return (
-    <div className='mt-8 flex'>
-      <button type='button' className='arrow-btn'>
+    <div className='mt-8 flex justify-end gap-x-2'>
+      <button
+        type='button'
+        className='arrow-btn'
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) {
+            prevPage = totalPages;
+          }
+          handlePageChange(prevPage);
+        }}
+      >
         <FaArrowLeft />
       </button>
-      <div>
-        {pageArr.map((item) => {
+      <div className='flex gap-x-1'>
+        {pageArr.map((pageNumber) => {
           return (
             <button
-              className={`page-btn ${
-                item === currentPage ? "active-btn" : ""
-              }`}
+              key={pageNumber}
+              className={`page-btn font-medium md:text-lg`}
+              onClick={() => handlePageChange(pageNumber)}
             >
-              {item}
+              {pageNumber}
             </button>
           );
         })}
       </div>
-      <button type='button' className='arrow-btn'>
+      <button
+        type='button'
+        className='arrow-btn'
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > totalPages) nextPage = 1;
+          handlePageChange(nextPage);
+        }}
+      >
         <FaArrowRight />
       </button>
     </div>
